ce8b4bb660e5022362ba9eedf7868eac355b7eb6
jose <2023159140@ub.edu.bz>
2025-03-03 16:27:29 -0600

    Initial commit - Task Manager Project

---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+import express from "express";
+
+const router = express.Router();
+import 'path';
+
+let tasks = [];
+let taskid = 1;
+
+router.get('/', (req, res)=>{
+    res.render('task', {tasks});
+});
+
+router.post('/add-task', (req, res)=>{
+    const {name, description} = req.body;
+    if(!name || !description){
+        return res.status(400).send("Name and descriptions are required");
+
+    }
+
+    tasks.push({name, description, id: taskid++, completed: false });
+    res.redirect('/');
+});
+router.post('/toggle-task/:id', (req, res)=>{
+    const task = tasks.find(t => t.id == req.params.id);
+    if(!task) {
+        return res.status(400).send("task not found");
+        
+    }
+    task.completed = !task.completed;
+    res.redirect('/');
+});
+
+router.post('/delete-task/:id', (req, res) => {
+    tasks = tasks.filter(t => t.id != req.params.id);
+    res.redirect('/');
+});
+
+export default router;
