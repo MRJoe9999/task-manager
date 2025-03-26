b7552fad8da0eaad0a29cbf87a8ef7168d6fca7d
jose <2023159140@ub.edu.bz>
2025-03-26 11:13:32 -0600

    added the config file

---
+++ models/subscriberModels.js
@@ -0,0 +1,13 @@
+//create me the schema for the subscriber
+import {query} from '../config/db.js';
+
+export const getAlltasks = async () =>{
+    try{
+        const result = await query('SELECT title, description FROM tasks');
+        return result.rows;
+    }
+    catch(err){
+        console.error("Error fetching tasks:", error);
+        throw error;
+    }
+}
